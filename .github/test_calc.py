387c729fc0cd6be04037a8b280e6e09b9e2f13a3
ajayyadav2181 <ajayyadav2181@gmail.com>
2026-04-17 21:18:06 +0530

    Python

--- .github/test_calc.py
+++ .github/test_calc.py
@@ -1,6 +1,6 @@
 import pytest
 
-from calculator import add,sub,mul,div
+from calculator import add, sub, mul, div,
 
 def test_add():
     assert add(2,3) == 5 # actual == expected (if match then test case will pass)
